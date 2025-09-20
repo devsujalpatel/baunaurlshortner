2e546f6ff7faefffcd2d40577032e6269f5194cd
devsujalpatel <sujal7455@gmail.com>
2025-09-20 16:21:31 +0530

    Oauth bug fixed

--- src/lib/actions/auth-actions.ts
+++ src/lib/actions/auth-actions.ts
@@ -1,26 +1,22 @@
 "use server";
 
 import { headers } from "next/headers";
-import { auth } from "../auth";
+import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 export const signOut = async () => {
-  const result = await auth.api.signOut({ headers: await headers() });
-  return result;
+  return await auth.api.signOut({ headers: await headers() });
 };
 
 export const signInSocial = async (provider: "google" | "github") => {
   const { url } = await auth.api.signInSocial({
     body: {
       provider,
-      callbackURL: "/shorten",
+      callbackURL: "/shorten", // after login, send user here
       requestSignUp: true,
-      
     },
     headers: await headers(),
   });
 
-  if (url) {
-    redirect(url);
-  }
+  if (url) redirect(url);
 };
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,24 +1,24 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db/db"; // your drizzle instance
-import { nextCookies } from "better-auth/next-js";
+import { db } from "@/db/db";
 import { schema } from "@/models/schema";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-    schema: schema,
+    schema,
   }),
   socialProviders: {
     github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     },
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
   plugins: [nextCookies()],
-  secret: process.env.BETTER_AUTH_SECRET as string,
+  secret: process.env.BETTER_AUTH_SECRET!,
 });
